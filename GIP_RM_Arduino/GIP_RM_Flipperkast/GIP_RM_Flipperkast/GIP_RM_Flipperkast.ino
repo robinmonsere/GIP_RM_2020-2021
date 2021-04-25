41e63174cf61db92578cba98e729f1e7fb567c39
xdNiBoR <51537026+xdNiBoR@users.noreply.github.com>
2021-04-25 13:41:42 +0200

    update

--- GIP_RM_Arduino/GIP_RM_Flipperkast/GIP_RM_Flipperkast/GIP_RM_Flipperkast.ino
+++ GIP_RM_Arduino/GIP_RM_Flipperkast/GIP_RM_Flipperkast/GIP_RM_Flipperkast.ino
@@ -4,17 +4,70 @@
 #include <Keypad.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+//////////////////////////////////////////////////////////////////////
+#define RST_PIN         49          // Zie pin layout in GIP_RM_RFID
+#define SS_PIN          53          // Zie pin layout in GIP_RM_RFID
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522
+String tag = "";
+String CurrentPlayer = "";
+String AllPlayers[13] = {"Admin" , "Blauw1" , "Blauw2" , "Blauw" , "Grijs" , "Zwart" , "Groen" , "Oranje" , "Rood" , "Geel" , "Paars" , "Wit"};
+String UIDtags[13] = {" 23 AA E9 1B" , " 5D 68 BD 02" , " 48 FF BF 02" , " 49 3D F8 62", " 21 05 22 03" , " 80 CB 21 03" , " 49 75 D4 02" , " A0 97 21 03" , " DC E8 1A 03" , " 60 7A D3 3D" , " C3 B4 21 03" , " D7 4B 21 03"};
+bool CorrectRFID = false;
+bool KeuzeMenu = true;
+bool Is_Ingelezen = false;
+//////////////////////////////////////////////////////////////////////
+LiquidCrystal_I2C lcd(0x27, 20,4);
 
-LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
-void setup() 
+const int flipper_1 = 9;
+const int flipper_2 = 10;
+const int drukknop_1 = 44;
+const int drukknop_2 = 45;
+
+bool stateF1 = LOW;
+bool stateF2 = LOW;
+bool isUsableF1 = true;
+bool isUsableF2 = true;
+
+//////////////////////////////////////////////////////////////////////
+byte Block = 60;                  // 60
+byte BufferSize = 18;
+byte ReadBuffer[18] = "";
+byte WriteBuffer[16] = "";
+int NCredits = 0;                //aantal Credits op de kaart.
+
+byte keuzemenuState = 0;
+
+unsigned long millisHTL1 = 0;
+unsigned long millisHTL2 = 0;
+unsigned long previousMillisF1 = 0;
+unsigned long previousMillisF2 = 0;
+unsigned long currentMillis = 0;
+const int timeBetweenUses = 500; // een halve seconde tussen elke flipperactie
+const int timerFlipper = 500;
+
+const byte joystickSW = 2;
+const byte joystickX = A0;
+const byte joystickY = A1;
+
+
+void setup()
 {
-  lcd.begin(16, 2);
+  lcd.init();
+  lcd.backlight();
+  SPI.begin();
+  Serial.begin(9600);
+  mfrc522.PCD_Init();
+  delay(4);
+  pinMode(flipper_1, INPUT);
+  pinMode(flipper_2, INPUT);
+  lcd.setCursor(1,1);
+
+  pinMode(joystickSW, INPUT_PULLUP);
   
-}
-void loop() {
-  // Print 'Hello World!' on the first line of the LCD:
-  lcd.setCursor(2, 0); // Set the cursor on the third column and first row.
-  lcd.print("Hello World!"); // Print the string "Hello World!"
-  lcd.setCursor(2, 1); //Set the cursor on the third column and the second row (counting starts at 0!).
-  lcd.print("LCD tutorial");
+ }
+
+void loop()
+{
+  if (CorrectRFID == false) RFIDSCAN();
+   
 }
---
+++ GIP_RM_Arduino/GIP_RM_Flipperkast/GIP_RM_Flipperkast/SPEL.ino
@@ -0,0 +1,71 @@
+void KIESSPEL()
+{
+  unsigned long cMillis = 0;
+  unsigned long cPreviousMillis = 0;
+  const int interval = 600;
+  bool OnOff = 0;
+
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("Normaal");
+  lcd.setCursor(1, 1);
+  lcd.print("Battle Royal");
+  lcd.setCursor(1, 2);
+  lcd.print("Keer terug");
+
+  while (1)
+  {
+    cMillis = millis();
+    if (cMillis - cPreviousMillis >= interval)
+    {
+      if (OnOff == 0)
+      {
+        lcd.setCursor(0, keuzemenuState);
+        lcd.print(">");
+        lcd.setCursor(19, keuzemenuState);
+        lcd.print("<");
+        cPreviousMillis = millis();
+      }
+      if (OnOff == 1)
+      {
+        lcd.setCursor(0, keuzemenuState);
+        lcd.print(" ");
+        lcd.setCursor(19, keuzemenuState);
+        lcd.print(" ");
+        cPreviousMillis = millis();
+      }
+      OnOff = !OnOff;
+    }
+
+    if (analogRead(joystickY) >= 750 and keuzemenuState != 0)
+    {
+      lcd.setCursor(0, keuzemenuState);
+      lcd.print(" ");
+      lcd.setCursor(19, keuzemenuState);
+      lcd.print(" ");
+      keuzemenuState = keuzemenuState - 1;
+      OnOff = 0;
+    }
+    if (analogRead(joystickY) <= 250 and keuzemenuState != 2)
+    {
+      lcd.setCursor(0, keuzemenuState);
+      lcd.print(" ");
+      lcd.setCursor(19, keuzemenuState);
+      lcd.print(" ");
+      keuzemenuState = keuzemenuState + 1;
+      OnOff = 0;
+    }
+    delay(300);
+    if (keuzemenuState == 2 and digitalRead(joystickSW) == LOW)
+    {
+      lcd.clear();
+      keuzemenuState = 0;
+      KEUZEMENU();
+    }
+    if (keuzemenuState == 0 and digitalRead(joystickSW) == LOW)
+    {
+      lcd.clear();
+      KIESSPEL();
+    }
+  }
+}
